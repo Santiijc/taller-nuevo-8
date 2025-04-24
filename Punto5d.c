d348d6a3be418511daba2f5b45ad7987e70dbc6d
hatucl <giraldojulian281@gmail.com>
2025-04-24 01:43:51 -0500

    putamare
    perras

---
+++ Punto5d.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    int n;
+    double fact;
+
+    printf("Ingrese el numero de terminos de la serie: ");
+    scanf("%d", &n)
+
+    for(int i = 1; i <= n; i++){
+        fact = 1;
+    }
+
+    return 0;
+}
